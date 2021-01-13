a6191cc673479ab49736a5564734d6d17d3f8867
XEROINSIDE\abhishek.kulkarni <abhishek.kulkarni@xero.com>
2021-01-13 14:37:45 +1300

    changes to the test

--- Tests/Utilities/CommonConstants.cs
+++ Tests/Utilities/CommonConstants.cs
@@ -8,18 +8,11 @@ namespace Tests.Utilities
         public static class DriverSettings
         {
             public static string filepath = Path.GetFileNameWithoutExtension("chromedriver.exe"); // Copy the chromedriver folder from git & pasteit under "C:\Users\yourusername\AppData\Local\Temp" for the web browser to initiate
-            //public static string BinaryLocationChrome = @"C:\Users\abhishek.kulkarni\My Folder\Practice\PracticeTest\Tests\Drivers\chromedriver_win32";
+            //public static string BinaryLocationChrome = @"C:\Users\abhishek.kulkarni\AppData\Local\Temp";
             public static string BinaryLocationChrome = filepath;
 
             public static string ChromeBrowser = "Chrome";
             public static int DefaultWaitTime = 3000;
-        }
-
-        public class Credentials
-        {
-            public static string Name { get; set; }
-            public static string PurchasableType { get; set; }
-            public static string PartnerOnly { get; set; }
         }
 
         public class ApplicationSettings
